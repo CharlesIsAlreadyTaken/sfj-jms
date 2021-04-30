73807eed9b369e4885594ecdeaababe04f400541
CharlesIsAlreadyTaken <charlesalbaut@gmail.com>
2021-04-30 07:54:44 -0400

    jms boilerplate

---
+++ src/main/java/guru/springframework/sfjjms/SfjJmsApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfjjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfjJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfjJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfjjms/SfjJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfjjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfjJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
