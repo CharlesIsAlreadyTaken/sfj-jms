c09d447127697f6a13dd68f2f5f5f77ab38beba8
CharlesIsAlreadyTaken <charlesalbaut@gmail.com>
2021-05-01 07:52:26 -0400

    send and receive messages

--- src/main/java/guru/springframework/sfjjms/listener/HelloListener.java
+++ src/main/java/guru/springframework/sfjjms/listener/HelloListener.java
@@ -2,20 +2,43 @@ package guru.springframework.sfjjms.listener;
 
 import guru.springframework.sfjjms.config.JmsConfig;
 import guru.springframework.sfjjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.Message;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
 public class HelloListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers, Message message){
         System.out.println("Message");
         System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                            @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
